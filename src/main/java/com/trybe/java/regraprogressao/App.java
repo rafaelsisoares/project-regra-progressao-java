fd72ce413e3a3842086285f42f730352b9c3cf28
Rafael Soares <r.soares2015@outlook.com>
2023-11-06 23:46:47 -0300

    feat: Criacao do loop para insercao das atividades

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,8 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -8,5 +11,22 @@ public class App {
   /**
    * Metodo main.
    */
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    ArrayList<String> avaliacoes = new ArrayList<String>();
+    ArrayList<Integer> peso = new ArrayList<Integer>();
+
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int numeroAtividades = input.nextInt();
+
+    for (int i = 1; i <= numeroAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + i + ": ");
+      avaliacoes.add(input.next());
+      input.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ": ");
+      peso.add(input.nextInt());
+    }
+
+    input.close();
+  }
 }
