3dca1ab3021395fc5e84d2dad425b5b7a1b57dba
Rafael Soares <r.soares2015@outlook.com>
2023-11-07 00:23:36 -0300

    feat: Adicionado a possibilidade de inserir notas para a atividade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -12,19 +12,21 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    ArrayList<String> avaliacoes = new ArrayList<String>();
+    Scanner input = new Scanner(System.in).useDelimiter("\n");
     ArrayList<Integer> peso = new ArrayList<Integer>();
+    ArrayList<Integer> notas = new ArrayList<Integer>();
 
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     int numeroAtividades = input.nextInt();
 
     for (int i = 1; i <= numeroAtividades; i++) {
       System.out.println("Digite o nome da atividade " + i + ": ");
-      avaliacoes.add(input.next());
+      final String atividadeAtual = input.next();
       input.nextLine();
       System.out.println("Digite o peso da atividade " + i + ": ");
       peso.add(input.nextInt());
+      System.out.println("Digite a nota obtida para " + atividadeAtual + ": ");
+      notas.add(input.nextInt());
     }
 
     input.close();
